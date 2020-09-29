fa35d174461d55f94ea640c613160532a7c75e6b
Marian Witkowski <marian.witkowski2@orange.com>
2020-09-29 16:14:35 +0200

    day 02

--- Dzien02/010-requests.py
+++ Dzien02/010-requests.py
@@ -1,4 +1,5 @@
 import requests as req
+from requests.auth import HTTPBasicAuth
 
 response = req.get("https://api.github.com/")
 if response.status_code==200:
@@ -20,3 +21,19 @@ json_reponse = response.json()
 #
 response = req.post("https://httpbin.org/post", data={"key":"value"})
 print(response.json())
+
+# basic auth
+url = "https://api.ambra.com.pl/j3GsmoZgcL/260/CQ02.png"
+response = req.get(url, auth=HTTPBasicAuth('service','alamakota'),
+                   allow_redirects=True, verify=False, timeout=(10, 15) )
+file_name = url.split("/")[-1]
+with open(file_name, "wb") as fd:
+    fd.write(response.content)
+print(response)
+
+print("="*80)
+# upload pliku
+with open("CQ02.png","rb") as fd:
+    file_dict = { "CQ02.png": fd }
+    response = req.post("https://httpbin.org/post", files=file_dict)
+    print(response.json())
